fedf36cff629a63907a4cf1bdb0e927939aea302
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-08 21:25:26 +0400

    Vera.Galstyan_Login_Page  Version 1

---
+++ vera.galstyan/selenium_ui/login_page/login_with_correct_credentials.py
@@ -0,0 +1,10 @@
+
+#This test case was created by Vera Galstyan
+
+#!/usr/bin/env python 3
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
+chrome_driver.get("https://www.saucedemo.com/inventory.html")
