bc84ac40a4967429c216afa30f4a1cc8e7d6d6fc
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-09 16:50:49 +0400

    Vera.Galstyan_Login_Page Test Case 1

--- vera.galstyan/selenium_ui/login_page/login_with_correct_credentials.py
+++ vera.galstyan/selenium_ui/login_page/login_with_correct_credentials.py
@@ -4,6 +4,9 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.common.by import By
+
 chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
 chrome_driver.get("https://www.saucedemo.com/inventory.html")
+chrome.driver.find_element(By.ID("user-name" ))
+chrome.drive.find_element(By.ID ("login-button" ))
